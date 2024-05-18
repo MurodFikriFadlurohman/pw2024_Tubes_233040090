90d067505d88a054e50378e4b9741fbbc28a1bfd
MurodFikriFadlurohman <murodadipate@gmail.com>
2024-05-18 08:28:06 +0700

    Telah menyelesaikan halaman utama dan navbar

--- halaman/navbar.php
+++ halaman/navbar.php
@@ -1,60 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Artsey Studio</title>
-    <!-- Css -->
-    <link rel="stylesheet" href="../css/navbar.css" />
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Artsey Studio</title>
+  <!-- Css -->
+  <link rel="stylesheet" href="../css/navbar.css" />
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Oswald:wght@200;400&family=Whisper&display=swap" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 
-    <!-- Link AOS -->
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Oswald:wght@200;400&family=Whisper&display=swap"
+    rel="stylesheet">
 
-    <!-- Boxicons -->
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <!-- Link AOS -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-  </head>
-  <body style="background-color: #d8d2d2;">
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top" style="font-family: Josefin Sans, sans-serif;">
-      <div class="container">
-        <a href=""class="text-decoration-none acer">Acer</a>
+  <!-- Boxicons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-        <!-- Search -->
+</head>
+
+<body style="background-color: #d8d2d2;">
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top" style="font-family: Josefin Sans, sans-serif;">
+    <div class="container">
+      <a href="" class="text-decoration-none acer">Acer</a>
+
+      <!-- Search -->
       <form action="">
         <div class="search">
           <label class="search-icon" for="search"><i class='bx bx-search'></i></label>
           <input type="search" name="search" placeholder="search" id="search">
         </div>
       </form>
-        <!-- Search -->
+      <!-- Search -->
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto gap-3">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#product">Product</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#profile">Kategori</a>
-              </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#team">Login</a>
-            </li>
-        </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto gap-3">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#product">Product</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <!-- akhir navbar -->
+    </div>
+  </nav>
+  <!-- akhir navbar -->
--- halaman/utama.php
+++ halaman/utama.php
@@ -1,11 +1,181 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Home</title>
+  <!-- Bootsrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- css -->
+  <link rel="stylesheet" href="../css/utama.css">
 </head>
+
 <body>
-    <h1>HAHHAHAHA</h1>
+  <?php 
+      require "navbar.php"
+    ?>
+
+  <!-- Conten 1 -->
+  <div class="content">
+    <div id="carouselExampleIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../image/acerSwift.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../image/acerPredator.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../image/acerNitro.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+  <!-- akhir Conten 1 -->
+
+  <div class="paragraf">
+    <h1>Telusuri Seri Laptop</h1>
+  </div>
+
+  <!-- Conten 2 -->
+  <div class="conten2">
+    <div class="card">
+      <img src="../image/aspire7.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Aspire</h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-secondary">Lihat Detail</a>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../image/swift2.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">swift</h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-secondary">Lihat Detail</a>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../image/predator2.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Gaming</h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-secondary">Lihat Detail</a>
+      </div>
+    </div>
+  </div>
+  < <!-- Akhir Conten 2 -->
+
+    <!-- Conten 3 -->
+    <div class="conten3">
+      <div class="card">
+        <img src="../image/nitro2.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Nitro</h5>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-secondary">Lihat Detail</a>
+        </div>
+      </div>
+      <div class="card">
+        <img src="../image/spin2.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Spin</h5>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-secondary">Lihat Detail</a>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Conten 3  -->
+
+    <!-- Conten 4 -->
+    <div class="conten4">
+      <div class="card-1" style="width: 18rem;">
+        <img src="../image/Windows 11.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4>Memperkenalkan Windows 11</h4>
+          <p class=" card-text">Windows 11 menghadirkan berbagai peningkatan dan fitur baru yang dirancang untuk
+            meningkatkan pengalaman pengguna. Sebelum mengupgrade, pastikan perangkat Anda memenuhi persyaratan sistem
+            dan pelajari lebih lanjut tentang fitur-fiturnya</p>
+        </div>
+      </div>
+
+      <div class="card-1" style="width: 18rem;">
+        <img src="../image/games pass.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4>Game Pass</h4>
+          <p class=" card-text">Game Pass di Windows 11 acer adalah pilihan yang tepat bagi para gamer PC yang ingin
+            mengakses pustaka game yang luas dengan harga yang terjangkau. Dengan berbagai fitur dan game berkualitas
+            tinggi, Game Pass menawarkan pengalaman bermain game yang tak terlupakan.</p>
+        </div>
+
+      </div>
+      <div class="card-1" style="width: 18rem;">
+        <img src="../image/orang.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4>Windows 11 pro</h4>
+          <p class=" card-text">Windows 11 Pro adalah sistem operasi yang kuat dan aman yang dirancang untuk memenuhi
+            kebutuhan bisnis dan profesional. Jika Anda memerlukan fitur keamanan dan manajemen perangkat tambahan,
+            Windows 11 Pro adalah pilihan yang tepat. Namun, jika Anda pengguna rumahan dengan kebutuhan komputasi
+            dasar, Windows 11 standar mungkin lebih sesuai.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Conten 4 -->
+
+    <!-- About us -->
+    <div class="about">
+      <div class="card-2">
+        <div class="card2-body">
+          <h5 class="card-title h5">About Us</h5>
+          <p class="card-text">Acer Web adalah situs web yang memaparkan penjualan laptop Acer terbaru dan terlengkap
+            di Indonesia. yang menawarkan berbagai macam pilihan laptop untuk memenuhi kebutuhan Anda, mulai dari laptop
+            untuk penggunaan sehari-hari, seperti browsing dan mengetik, hingga laptop gaming berperforma tinggi dan
+            laptop tipis dan ringan yang sempurna untuk mobilitas.</p>
+        </div>
+      </div>
+
+      <div class="card-2">
+        <div class="card2-body">
+          <h5 class="card-title h5">laptop series</h5>
+          <p class="card-text">Jika Anda sedang mencari laptop gaming, maka Seri
+            Laptop Acer Predator dan Nitro adalah pilihan yang tepat untuk Anda. Jelajahi berbagai model yang tersedia
+            dan
+            temukan laptop yang sesuai dengan kebutuhan dan gaya Anda.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir About us -->
+
+    <!-- Footer -->
+    <div class="footer">
+      <p>© 2024 Acer Inc.</p>
+    </div>
+    <!-- Akhir footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
